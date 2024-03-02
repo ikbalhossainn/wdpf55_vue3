e3df90244e4be9dfc294f2f4714797becb0bedaf
ikbalhossainn <ikbalhossain125@gmail.com>
2024-03-02 13:07:28 +0600

    updated

---
+++ project1/src/components/Fruits.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                fruits: ['Apple', 'Orange', 'Grape', 'Lichi']
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
