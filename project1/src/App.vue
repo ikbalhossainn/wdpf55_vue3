87daf422c23e06573ab71b276da4ef3f4b73ece0
ikbalhossainn <ikbalhossain125@gmail.com>
2024-03-02 12:36:54 +0600

    updated

--- project1/src/App.vue
+++ project1/src/App.vue
@@ -1,31 +1,42 @@
-<script>
-
-</script>
+<script></script>
 
 <template>
-  <header-item/>
-  <div>
-    <h1> Hello Dear </h1>
-  <h2> {{ msg }}</h2>
-  <food-item/>
+  <header-item />
+  <div class="body">
+    <h1>Hello Dear</h1>
+    <h2>{{ msg }}</h2>
+    <food-item />
+    <food-item
+      food-name="Apple"
+      food-details="Here goes details content about Apple"
+    />
+    <food-item
+      food-name="Mango"
+      food-details="Here goes details content about Mango"
+    />
+    <food-item
+      food-name="Orange"
+      food-details="Here goes details content about Orange"
+    />
   </div>
 </template>
 
 <script>
-  export default {
-    data(){
-      return{
-        msg: "Welcome to my Home "
-      }
-      
-    }
-  }
+export default {
+  data() {
+    return {
+      msg: "Welcome to my Home",
+    };
+  },
+};
 </script>
 
-
 <style>
-  h1{
-    color: coral;
-    font-weight: bolder;
-  }
+h1 {
+  color: coral;
+  font-weight: bolder;
+}
+.body {
+  text-align: center;
+}
 </style>
--- project1/src/components/FoodItem.vue
+++ project1/src/components/FoodItem.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="box">
+    <div class="box" v-on:click="foodclick">
         <h1>{{ message }}</h1>
+        <h2>{{ foodName }}</h2>
+        <p>{{ foodDetails }}</p>
+        <span> {{ clicks }} times click</span>
 
     </div>
 </template>
@@ -9,10 +12,21 @@
     export default{
         data(){
             return{
-                message: "How beautiful you are!"
+                message: "How beautiful you are!",
+                clicks: 0
             }
+        },
+    
+    props: [
+        'foodName',
+        'foodDetails'
+    ],
+    methods:{
+        foodclick(){
+            this.clicks++
         }
     }
+}
 </script>
 
 <style>
