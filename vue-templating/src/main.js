d55dd60dc66deed501a9f36f04e3463cd0ae89b9
ikbalhossainn <ikbalhossain125@gmail.com>
2024-03-04 13:18:09 +0600

    updated

--- vue-templating/src/main.js
+++ vue-templating/src/main.js
@@ -2,5 +2,30 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from "./components/Home.vue";
+import Departments from "./components/Departments.vue";
+import AboutUs from "./components/AboutUs.vue";
+import Doctors from "./components/Doctors.vue";
+import Contact from "./components/Contact.vue";
+import Testimonials from "./components/Testimonials.vue";
 
-createApp(App).mount('#app')
+
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: Home },
+        { path: '/about', component: AboutUs },
+        { path: '/doctors', component: Doctors },
+        { path: '/departments', component: Departments },
+        { path: '/contact', component: Contact },
+        { path: '/testimonials', component: Testimonials },
+        
+    ]
+});
+
+const app = createApp(App)
+app.use(router);
+app.mount('#app')
